febe62cf44538d0bae071cbf9232a4f9183e0376
Nguyen Hung <hung@cafe24corp.vn>
2023-04-20 11:49:18 +0700

    bookstore-laravel

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['request']->server->set('HTTPS', true);
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->isLocal()) 
+        {
+            $this->app['request']->server->set('http', true);
+        } 
+        else 
+        {
+            $this->app['request']->server->set('https', true);
+        }
+    }
+}
